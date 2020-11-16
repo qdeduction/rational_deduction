ce5ca366677f229ed0399b71d360fe83fe5e3487
Brandon Gomes <bhgomes.code@gmail.com>
2020-11-15 19:04:38 -0500

    project restructuring

--- src/lib.rs
+++ src/lib.rs
@@ -1,114 +1,12 @@
 // file: src/lib.rs
 // authors: Brandon H. Gomes
 
-//! The Rational Deduction Algorithm
-
-use exprz_core::*;
+//! Rational Deduction
 
 use {
     core::{convert::TryFrom, iter::FromIterator},
-    std::vec::Vec,
+    exprz_core::{Expr, Expression},
 };
-
-/// Substitution Trait
-pub trait Substitution<A, B>
-where
-    A: Expression,
-    B: Expression,
-{
-    /// Compute the substitution on atoms.
-    fn on_atoms(&self, atom: A::Atom) -> B;
-
-    /// Compute the substitution on groups.
-    fn on_groups(&self, group: A::Group) -> B::Group {
-        // FIXME: group.into_iter().map(move |e| self.on_exprs(e)).collect()
-        let _ = group;
-        todo!()
-    }
-
-    /// Compute the substitution on expressions.
-    fn on_exprs(&self, expr: A) -> B {
-        // FIXME:
-        // match expr.cases() {
-        //     Expr::Atom(atom) => self.on_atoms(atom),
-        //     Expr::Group(group) => B::from_group(self.on_groups(group)),
-        // }
-        let _ = expr;
-        todo!()
-    }
-}
-
-impl<A, B, F> Substitution<A, B> for F
-where
-    A: Expression,
-    B: Expression,
-    F: Fn(A::Atom) -> B,
-{
-    #[inline]
-    fn on_atoms(&self, atom: A::Atom) -> B {
-        self(atom)
-    }
-}
-
-/// Generator for substitution using an iterator.
-#[inline]
-pub fn substitute_iter_on_atoms<'s, E, I>(iter: I, atom: E::Atom) -> E
-where
-    E: 's + Expression,
-    E::Atom: PartialEq,
-    I: IntoIterator<Item = (&'s E::Atom, E)>,
-{
-    iter.into_iter()
-        .find(|(a, _)| **a == atom)
-        .map(move |(_, t)| t)
-        .unwrap_or_else(move || E::from_atom(atom))
-}
-
-/// Compute the symmetric difference of two multisets.
-pub fn multiset_symmetric_difference<L, OL>(
-    left: L,
-    right: Vec<L::Item>,
-) -> (OL, impl Iterator<Item = L::Item>)
-where
-    L: IntoIterator,
-    L::Item: PartialEq,
-    OL: FromIterator<L::Item>,
-{
-    multiset_symmetric_difference_by(left, right, move |l, r| l == r)
-}
-
-/// Compute the symmetric difference of two multisets.
-pub fn multiset_symmetric_difference_by<L, OL, F>(
-    left: L,
-    right: Vec<L::Item>,
-    mut eq: F,
-) -> (OL, impl Iterator<Item = L::Item>)
-where
-    L: IntoIterator,
-    OL: FromIterator<L::Item>,
-    F: FnMut(&L::Item, &L::Item) -> bool,
-{
-    let right_len = right.len();
-    let mut matched_indices = Vec::<bool>::with_capacity(right_len);
-    matched_indices.resize(right_len, false);
-    (
-        left.into_iter()
-            .filter(|l| {
-                (&right).iter().enumerate().all(|(i, r)| {
-                    if eq(l, r) && !matched_indices[i] {
-                        matched_indices[i] = true;
-                        return false;
-                    }
-                    true
-                })
-            })
-            .collect(),
-        right
-            .into_iter()
-            .zip(matched_indices)
-            .filter_map(move |(r, m)| Some(r).filter(|_| !m)),
-    )
-}
 
 /// Ratio Trait
 pub trait Ratio<T, V>
@@ -125,6 +23,12 @@ where
         Self::new(pair.top, pair.bot)
     }
 
+    /// Reverse a `Ratio`.
+    #[inline]
+    fn reverse(self) -> Self {
+        Self::from_pair(self.into().reverse())
+    }
+
     /// Get the default ratio.
     #[inline]
     fn default() -> Self {
@@ -132,6 +36,7 @@ where
     }
 
     /// Compose two ratios using the ratio monoid multiplication algorithm.
+    #[inline]
     fn pair_compose(top: Self, bot: Self) -> Self
     where
         T: PartialEq,
@@ -146,7 +51,7 @@ where
     {
         let top = top.into();
         let bot = bot.into();
-        let (lower, upper) = multiset_symmetric_difference_by::<_, V, _>(
+        let (lower, upper) = util::multiset_symmetric_difference_by::<_, V, _>(
             top.bot,
             bot.top.into_iter().collect(),
             &mut eq,
@@ -160,6 +65,7 @@ where
     /// Fold a collection of ratios using [`pair_compose`].
     ///
     /// [`pair_compose`]: trait.Ratio.html#method.pair_compose
+    #[inline]
     fn compose<I>(ratios: I) -> Self
     where
         I: IntoIterator<Item = Self>,
@@ -180,18 +86,6 @@ where
         iter.next()
             .map(move |r| iter.fold(r, move |t, b| Self::pair_compose_by(t, b, &mut eq)))
             .unwrap_or_else(Self::default)
-    }
-
-    /// Perform a substitution over the ratio.
-    fn substitute<F>(self, f: F) -> Self
-    where
-        T: Expression<Group = V>,
-        V: iter::IntoIteratorGen<T>,
-        F: FnMut(T::Atom) -> Self,
-    {
-        let _ratio = self.into();
-        let _ = f;
-        todo!()
     }
 }
 
@@ -222,16 +116,6 @@ where
     }
 }
 
-impl<T, V> Ratio<T, V> for RatioPair<T, V>
-where
-    V: IntoIterator<Item = T> + FromIterator<T>,
-{
-    #[inline]
-    fn new(top: V, bot: V) -> Self {
-        Self { top, bot }
-    }
-}
-
 impl<T, V> Default for RatioPair<T, V>
 where
     V: IntoIterator<Item = T> + FromIterator<T>,
@@ -245,15 +129,45 @@ where
     }
 }
 
-/// `RatioPair` over an `Expression` type
-pub type RatioPairExpr<E> = RatioPair<E, <E as Expression>::Group>;
+impl<T, V> Ratio<T, V> for RatioPair<T, V>
+where
+    V: IntoIterator<Item = T> + FromIterator<T>,
+{
+    #[inline]
+    fn new(top: V, bot: V) -> Self {
+        Self { top, bot }
+    }
+}
 
-impl<E> From<RatioPairExpr<E>> for Expr<E>
+impl<T, V> Into<RatioPair<T, V>> for (V, V)
+where
+    V: IntoIterator<Item = T> + FromIterator<T>,
+{
+    #[inline]
+    fn into(self) -> RatioPair<T, V> {
+        RatioPair::new(self.0, self.1)
+    }
+}
+
+impl<T, V> Ratio<T, V> for (V, V)
+where
+    V: IntoIterator<Item = T> + FromIterator<T>,
+{
+    #[inline]
+    fn new(top: V, bot: V) -> Self {
+        (top, bot)
+    }
+}
+
+impl<E> From<RatioPair<E, E::Group>> for Expr<E>
 where
     E: Expression,
     E::Group: IntoIterator<Item = E> + FromIterator<E>,
 {
-    fn from(ratio: RatioPairExpr<E>) -> Self {
+    /// Convert a `RatioPairExpr<E>` into an `Expr<E>` by forgetting the shape of the underlying
+    /// expression.
+    #[inline]
+    fn from(ratio: RatioPair<E, E::Group>) -> Self {
         Self::Group(
             Some(E::from_group(ratio.top))
                 .into_iter()
@@ -263,62 +177,190 @@ where
     }
 }
 
-/// Conversion from `Expr` to `RatioPair` Error Type
-#[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
-pub enum RatioPairFromExprError {
-    /// The expression is not a group.
-    NotGroup,
-
-    /// The expression has the wrong group shape.
-    BadGroupShape,
-
-    /// The top element of the group is not a group.
-    MissingTopGroup,
-
-    /// The bot element of the group is not a group.
-    MissingBotGroup,
-
-    /// The top and bot element of the group are not groups.
-    MissingTopBotGroup,
-}
-
-impl<E> TryFrom<Expr<E>> for RatioPairExpr<E>
+impl<E> TryFrom<Expr<E>> for RatioPair<E, E::Group>
 where
     E: Expression,
     E::Group: IntoIterator<Item = E> + FromIterator<E>,
 {
-    type Error = RatioPairFromExprError;
+    type Error = expr::RatioPairFromExprError;
 
+    /// Parse an `Expr<E>` into a `RatioPairExpr<E>` if it has the correct shape.
     fn try_from(expr: Expr<E>) -> Result<Self, Self::Error> {
         match expr {
-            Expr::Atom(_) => Err(RatioPairFromExprError::NotGroup),
+            Expr::Atom(_) => Err(expr::RatioPairFromExprError::NotGroup),
             Expr::Group(group) => {
-                let mut group = group.into_iter();
-                if let (Some(top), Some(bot), None) = (group.next(), group.next(), group.next()) {
+                let mut iter = group.into_iter();
+                if let (Some(top), Some(bot), None) = (iter.next(), iter.next(), iter.next()) {
                     match (top.into(), bot.into()) {
                         (Expr::Group(top), Expr::Group(bot)) => Ok(Self { top, bot }),
-                        (_, Expr::Group(_)) => Err(RatioPairFromExprError::MissingTopGroup),
-                        (Expr::Group(_), _) => Err(RatioPairFromExprError::MissingBotGroup),
-                        _ => Err(RatioPairFromExprError::MissingTopBotGroup),
+                        (_, Expr::Group(_)) => Err(expr::RatioPairFromExprError::MissingTopGroup),
+                        (Expr::Group(_), _) => Err(expr::RatioPairFromExprError::MissingBotGroup),
+                        _ => Err(expr::RatioPairFromExprError::MissingTopBotGroup),
                     }
                 } else {
-                    Err(RatioPairFromExprError::BadGroupShape)
+                    Err(expr::RatioPairFromExprError::BadGroupShape)
                 }
             }
         }
     }
 }
 
-/// Evaluate a composition by performing each substitution and then composing ratios.
-pub fn eval_composition<'t, E, R, S, I>(terms: I) -> R
-where
-    E: Expression + PartialEq,
-    E::Group: IntoIterator<Item = E> + FromIterator<E>,
-    R: Ratio<E, E::Group>,
-    S: 't + Substitution<E, E>,
-    I: IntoIterator<Item = (R, &'t S)>,
-{
-    let _ = terms;
-    // FIXME: Ratio::compose(terms.into_iter().map(move |(r, s)| r.substitute(s)))
-    todo!()
+/// Expression Ratio Module
+pub mod expr {
+    use {
+        super::Ratio,
+        core::iter::FromIterator,
+        exprz_core::{ExprRef, Expression},
+    };
+
+    /// Conversion from `Expr` to `RatioPair` Error Type
+    #[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
+    pub enum RatioPairFromExprError {
+        /// The expression is not a group.
+        NotGroup,
+
+        /// The expression has the wrong group shape.
+        BadGroupShape,
+
+        /// The top element of the group is not a group.
+        MissingTopGroup,
+
+        /// The bot element of the group is not a group.
+        MissingBotGroup,
+
+        /// The top and bot element of the group are not groups.
+        MissingTopBotGroup,
+    }
+
+    /// Check if an `Expression` has the right shape to be a ratio.
+    ///
+    /// Use [`try_from`] to convert an `Expr<E>` to a `RatioPairExpr<E>`.
+    ///
+    /// [`try_from`]: ../struct.RatioPair.html#impl-TryFrom<Expr<E>>
+    #[must_use]
+    pub fn has_ratio_shape<E>(expr: &E) -> bool
+    where
+        E: Expression,
+    {
+        match expr.cases() {
+            ExprRef::Group(group) => {
+                use {core::borrow::Borrow, exprz_core::iter::IteratorGen};
+                let mut iter = group.iter();
+                if let (Some(top), Some(bot), None) = (iter.next(), iter.next(), iter.next()) {
+                    top.borrow().is_group() && bot.borrow().is_group()
+                } else {
+                    false
+                }
+            }
+            _ => false,
+        }
+    }
+
+    /// Substitute an `Expression` into each `Atom` of `self`.
+    #[inline]
+    pub fn substitute<E, R, F>(ratio: R, mut f: F) -> R
+    where
+        E: Expression,
+        E::Group: IntoIterator<Item = E> + FromIterator<E>,
+        R: Ratio<E, E::Group>,
+        F: FnMut(E::Atom) -> E,
+    {
+        let ratio = ratio.into();
+        Ratio::new(
+            ratio
+                .top
+                .into_iter()
+                .map(|e| e.substitute(&mut f))
+                .collect(),
+            ratio
+                .bot
+                .into_iter()
+                .map(|e| e.substitute(&mut f))
+                .collect(),
+        )
+    }
+
+    /// Evaluate a composition by performing each substitution and then composing ratios.
+    #[inline]
+    pub fn eval_composition<E, R, F, S, I>(terms: I) -> R
+    where
+        E: Expression + PartialEq,
+        E::Group: IntoIterator<Item = E> + FromIterator<E>,
+        R: Ratio<E, E::Group>,
+        F: FnMut(E::Atom) -> E,
+        S: AsMut<F>,
+        I: IntoIterator<Item = (R, S)>,
+    {
+        Ratio::compose(
+            terms
+                .into_iter()
+                .map(move |(r, mut s)| substitute(r, s.as_mut())),
+        )
+    }
+}
+
+/// Utilities
+pub mod util {
+    use {core::iter::FromIterator, exprz_core::Expression};
+
+    /// Compute the symmetric difference of two multisets.
+    #[inline]
+    pub fn multiset_symmetric_difference<L, OL>(
+        left: L,
+        right: Vec<L::Item>,
+    ) -> (OL, impl Iterator<Item = L::Item>)
+    where
+        L: IntoIterator,
+        L::Item: PartialEq,
+        OL: FromIterator<L::Item>,
+    {
+        multiset_symmetric_difference_by(left, right, move |l, r| l == r)
+    }
+
+    /// Compute the symmetric difference of two multisets.
+    pub fn multiset_symmetric_difference_by<L, OL, F>(
+        left: L,
+        right: Vec<L::Item>,
+        mut eq: F,
+    ) -> (OL, impl Iterator<Item = L::Item>)
+    where
+        L: IntoIterator,
+        OL: FromIterator<L::Item>,
+        F: FnMut(&L::Item, &L::Item) -> bool,
+    {
+        let right_len = right.len();
+        let mut matched_indices = Vec::<bool>::with_capacity(right_len);
+        matched_indices.resize(right_len, false);
+        (
+            left.into_iter()
+                .filter(|l| {
+                    (&right).iter().enumerate().all(|(i, r)| {
+                        if eq(l, r) && !matched_indices[i] {
+                            matched_indices[i] = true;
+                            return false;
+                        }
+                        true
+                    })
+                })
+                .collect(),
+            right
+                .into_iter()
+                .zip(matched_indices)
+                .filter_map(move |(r, m)| Some(r).filter(|_| !m)),
+        )
+    }
+
+    /// Generator for substitution using an iterator.
+    #[inline]
+    pub fn substitute_iter_on_atoms<'s, E, I>(iter: I, atom: E::Atom) -> E
+    where
+        E: 's + Expression,
+        E::Atom: PartialEq,
+        I: IntoIterator<Item = (&'s E::Atom, E)>,
+    {
+        iter.into_iter()
+            .find(|(a, _)| **a == atom)
+            .map(move |(_, t)| t)
+            .unwrap_or_else(move || E::from_atom(atom))
+    }
 }
